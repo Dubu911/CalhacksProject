196c81334cac5085a0706746a6dd4d4d759ad554
Leo Herrera <93077720+yleoherrera@users.noreply.github.com>
2022-10-15 22:10:48 -0700

    gravity pt 6

--- function.py
+++ function.py
@@ -1,10 +1,22 @@
 
+import random
+
+
 def gravity(x):
     if x == 0:
         y = 1
     y = x * 1.1
-    if y >= 640:
-        y = 640
+
+    if 640 > y > 630:
+        y = 630
+        z = random.randrange(1, 4)
+        if z > 1:
+            y = 0
+            return y
+    if y > 640:
+        y = 0
     return y
 
 
+z = random.randrange(1, 4)
+print(z)
