cd71d4301edd2602253c79279148e2fbfcd36c51
dubu911 <daeyoungdykim@gmail.com>
2022-10-15 20:52:32 -0700

    add salad

--- main.py
+++ main.py
@@ -26,12 +26,18 @@ character_size = character.get_rect().size # get the size of original image. rec
 character_width = character_size[0]
 character_height = character_size[1]
 character_x_pos = screen_width/3*1.3
-character_y_pos = screen_height/2*1.6
+character_y_pos = screen_height/2*1.75
 
 # location to move
 to_x = 0
 to_y = 0
 
+salad = pygame.image.load(os.path.join(game_dir, "salad.png")).convert()
+salad_size = character.get_rect().size # get the size of original image. rectangle
+salad_width = salad_size[0]
+salad_height = salad_size[1]
+salad_x_pos = screen_width
+salad_y_pos = screen_height
 
 # event roop - to keep the display up there
 running = True # to check if the game is running
@@ -43,38 +49,32 @@ while running :
         
         if event.type == pygame.KEYDOWN : # to check if the key is pressed
             if event.key == pygame.K_LEFT : # to move the character to the left
-                to_x -= .5
+                to_x -= .3
             elif event.key == pygame.K_RIGHT : # to move the character to the right
-                to_x += .5
-            elif event.key == pygame.K_UP : # to move the character toward up
-                to_y -= .5
-            elif event.key == pygame.K_DOWN : # to move the character toward down
-                to_y += .5
+                to_x += .3
         
         if event.type == pygame.KEYUP : # to check if the key is not pressed
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT :
                 to_x = 0
-            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN :
-                to_y = 0
+            # elif event.key == pygame.K_UP or event.key == pygame.K_DOWN :
+            #     to_y = 0
 
     character_x_pos += to_x
     character_y_pos += to_y
+
+    salad_x_pos = screen_width
+    salad_y_pos = screen_height
 
     # set a limit of movement on X position.
     if character_x_pos < 0 :
         character_x_pos = 0
     elif character_x_pos > screen_width - character_width :
         character_x_pos = screen_width - character_width
-    # set a limit of movement on Y position.
-    if character_y_pos < 0 :
-        character_y_pos = 0
-    elif character_y_pos > screen_height - character_height :
-        character_y_pos = screen_height-character_height
  
     screen.blit(background,(0,0)) # add background information
     
     screen.blit(character, (character_x_pos,character_y_pos)) # character create 
-    
+    screen.blit(salad, (salad_x_pos,salad_y_pos))
     
     
     pygame.display.update() # to update the display on every single frame.
